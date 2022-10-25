ad209ab71991088d8120f3d866c4d39d1041f863
Fede <fede.collasius@gmail.com>
2022-10-25 20:27:54 -0300

    Emprolijo

--- src/equipo.cpp
+++ src/equipo.cpp
@@ -1,7 +1,9 @@
 #include "equipo.h"
+/**/
 #include <unistd.h>
 #include <atomic>
 #include <semaphore.h>
+/**/
 
 
 direccion Equipo::apuntar_a(coordenadas pos1, coordenadas pos2) {
@@ -26,27 +28,29 @@ void Equipo::jugador(int nro_jugador) {
 	while(!this->belcebu->termino_juego()) { // Chequear que no haya una race condition en gameMaster
 		switch(this->strat) {
 			//SECUENCIAL,RR,SHORTEST,USTEDES
-			case(SECUENCIAL):
-				// NO ESPERA NI ASU MADRE
-				// ...
-				
+			case(SECUENCIAL): // No espera a nadie. 
+				// 
+				// 
 				//
 				break;
 			
-			case(RR):
-				// ESPERA A QUE LE TOQUE NRO = MOVIDOS
-				// ...
+			case(RR): // Espera a que le toque nro = movidos. 
+				/**/
 				if(nro_jugador==(jugadores_movidos_esta_ronda+1)%cant_jugadores){
 					if(equipo == ROJO){
-						belcebu->mover_jugador(apuntar_a(pos_bandera_azul.first, pos_bandera_azul.second), nro_jugador)
+						belcebu->mover_jugador(apuntar_a(posActualJugador, pos_bandera_azul), nro_jugador);
 					} else {
-						belcebu->mover_jugador(apuntar_a(pos_bandera_roja.first, pos_bandera_roja.second), nro_jugador)
+						belcebu->mover_jugador(apuntar_a(posActualJugador, pos_bandera_roja), nro_jugador);
 					}
 				}
-				//
+				/**/
 				break;
 
-			case(SHOde N
+			case(SHORTEST): 
+				//
+				//	...
+				//
+				break;
 			case(USTEDES):
 				//
 				// ...
@@ -63,14 +67,16 @@ void Equipo::jugador(int nro_jugador) {
 	}
 	
 }
-
+/**/
 // Variables globales para simplificar el uso de memoria compartida
 atomic<int> jugadores_movidos_esta_ronda(0);
 atomic<int> nro_jugador_mas_cercano_rojo(0);
 atomic<int> nro_jugador_mas_cercano_azul(0);
 sem_t bandera_roja_encontrada, bandera_azul_encontrada;
-atomic<coordenadas> pos_bandera_azul, pos_bandera_roja;
+// Sacamos la parte atomica de las banderas porque no es necesario que sea atomica para el problema que planteamos (no hay dos equipos que quieran modificar la misma bandera a la vez) 
+coordenadas pos_bandera_azul, pos_bandera_roja;
 vector<coordenadas> posiciones_azul, posiciones_rojo;
+/**/
 
 Equipo::Equipo(gameMaster *belcebu, color equipo, 
 		estrategia strat, int cant_jugadores, int quantum, vector<coordenadas> posiciones) {
@@ -82,15 +88,12 @@ Equipo::Equipo(gameMaster *belcebu, color equipo,
 	this->quantum = quantum;
 	this->quantum_restante = quantum;
 	this->cant_jugadores = cant_jugadores;
+	/**/
 	//this->posiciones = posiciones;
-	//
-	// ...
-/**/vector<int[2]> los_tubos;
+	vector<int[2]> los_tubos;
 	for (int i = 0; i < cant_jugadores; i++) {
 		pipe(los_tubos[i]);
 	}
-	//
-/**/
 	if (equipo == ROJO) {
 		sem_init(&bandera_roja_encontrada, 1337/*nonzero*/, cant_jugadores);
 		posiciones_rojo = posiciones;
@@ -98,19 +101,21 @@ Equipo::Equipo(gameMaster *belcebu, color equipo,
 		sem_init(&bandera_azul_encontrada, 1337/*nonzero*/, cant_jugadores);
 		posiciones_azul = posiciones;
 	}
+	/**/
 }
 
 void Equipo::comenzar() {
 	// Arranco cuando me toque el turno 
 	// TODO: Quien empieza ? 
-/**/(equipo == ROJO ? belcebu->turno_rojo : belcebu->turno_azul).lock(); // espero mi turno
+	/**/
+	(equipo == ROJO ? belcebu->turno_rojo : belcebu->turno_azul).lock(); // Espero mi turno. 
 	//
-	// ...	//
-	
+	// ...	
+	//
 	// Creamos los jugadores
 	for(int i=0; i < cant_jugadores; i++) {
 		jugadores.emplace_back(thread(&Equipo::jugador, this, i)); 
-		//cerrar pipes?'????'''?'''!!!?!?!?!?!?!?!?!?!?!¿
+		//Cerramos pipes?????????????????????????????
 	}
 
 	buscar_bandera_contraria(); // van a volar signals a rolete
@@ -128,7 +133,8 @@ coordenadas Equipo::buscar_bandera_contraria() {
 	// ...
 	//
 
-	/**/ // dejar que empiezen a jugar todos
+	/**/ 
+	// Dejar que empiezen a jugar todos.
 	for (int pp = 0; pp < cant_jugadores; pp++) {
 		if (equipo == ROJO) {
 			sem_post(&bandera_roja_encontrada);
